a8ee92ca4b94807c9b125a17b7655a4bee0bea78
pestex-code <damian@pestex.pl>
2023-01-12 01:11:29 +0100

    2 dzień nauki, aktualizacja

--- program.py
+++ program.py
@@ -1,1 +1,11 @@
+import json
 
+zycia = 5
+
+with open("hasla.json") as hasla_json:
+    hasla = json.load(hasla_json)
+
+
+#print(hasla[i]["haslo"])
+while True:
+    litera = input("Wpisz literę: ")
